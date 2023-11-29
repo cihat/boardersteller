efa0baba751bee6b6f7005299d13ea1d0907d228
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2023-11-30 01:55:42 +0300

    Add Builder layout and editor page

---
+++ src/pages/editor.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'EditorPage',
+})
+</script>
+
+<template>
+  <div>
+    <h1>TEST</h1>
+  </div>
+</template>
+
+<route lang="yaml">
+  meta:
+    layout: builder
+</route>
